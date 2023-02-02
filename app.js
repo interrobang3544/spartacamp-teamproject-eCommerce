813560f1963c9f3a30370f2c659ecd58dca62c53
rsl150101 <dnwnd501@naver.com>
2023-02-03 02:50:41 +0900

    서버 구축, 세팅 body 데이터 해석을 위한 미들웨어 설정, 라우터 경로 설정

---
+++ app.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const basketRouter = require('./routes/baskets.route');
+
+//* express 할당
+const app = express();
+
+//* 포트 번호
+const PORT = 6000;
+
+//* body 데이터를 해석하기 위해 전역 미들웨어 설정
+app.use(express.json()); //- JSON 형태의 데이터 해석
+app.use(express.urlencoded({ extended: false })); //- x-www-form-urlencoded 형태 데이터 해석
+//? extended 속성이 true 면 qs 모듈 필요, false 면 NodeJS 기본 내장 querystring 모듈 사용
+
+//* 라우터 설정
+app.use('/baskets', basketRouter);
+
+//* 서버 리슨
+app.listen(PORT, () => {
+  console.log(`✅ 서버가 연결되었습니다. ${PORT}`);
+});
