47f6d5decbca09826bb72f28c0af7250cea55d4b
redfaceman <118166327+redfaceman@users.noreply.github.com>
2023-02-03 19:37:49 +0900

    홈페이지 데이터 연결

--- app.js
+++ app.js
@@ -1,11 +1,20 @@
 const express = require('express');
 const app = express();
+const ejs = require('ejs');
 const port = 4000;
 
 const routes = require('./routes/api.route');
 
 app.use(express.json());
-app.use('/api', routes);
+app.use(routes);
+app.use(express.static("static"));
+
+app.set('view engine', 'ejs');
+app.set('views', './views');
+
+app.get('/', (req, res) => {
+  res.render('home');
+})
 
 app.listen(port, () => {
   console.log(port, '포트로 서버가 열렸어요!');
