0ee5db0ab3da98fba2fba32f35eef392c8f4827c
Jeongjiw00 <wldn0229@naver.com>
2023-02-03 16:59:40 +0900

    fix:error

--- app.js
+++ app.js
@@ -6,8 +6,8 @@ const adminRouter = require('./routes/admin.routes');
 const apiRouter = require('./routes/api.routes');
 const usersRouter = require('./routes/users.routes');
 
-app.set('view engine', 'ejs')
-app.set('views', './views')
+app.set('view engine', 'ejs');
+app.set('views', './views');
 
 app.use(express.static('static'));
 app.use(express.json());
@@ -16,13 +16,17 @@ app.use('/admin', adminRouter);
 app.use('/api/auth', apiRouter);
 app.use('/users', usersRouter);
 
+app.get('/login', (req, res) => {
+  res.render('login');
+});
+
 app.get('/admin-users', (req, res) => {
-  res.render('admin-users')
-})
+  res.render('admin-users');
+});
 
 app.get('/admin-products', (req, res) => {
-  res.render('admin-products')
-})
+  res.render('admin-products');
+});
 
 app.listen(port, () => {
   console.log(port, '포트로 서버가 켜졌어요!');
