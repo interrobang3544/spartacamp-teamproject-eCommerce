2cce04669a9fab3a486b1f5bb6da9463c190a1c9
Joon Ki Chun <joon0858@naver.com>
2023-02-06 18:37:49 +0900

    Feat user search

--- controllers/users.controller.js
+++ controllers/users.controller.js
@@ -93,10 +93,17 @@ class UsersController {
   adminGetUsersBySearchWord = async (req, res, next) => {
     const { searchword } = req.params;
     try {
+      let limit = 5;
+      let offset = 0 + (req.query.page - 1) * limit;
       const usersInfo = await this.userService.adminFindUsersBySearchWord(
+        limit,
+        offset,
         searchword
       );
-      return res.status(200).json({ data: usersInfo });
+      return res.status(200).json({
+        totalPage: Math.ceil(usersInfo.count / limit),
+        data: usersInfo.rows,
+      });
     } catch (error) {
       return res.status(400).json({
         errorMessage: '회원 정보 조회에 실패하였습니다.',
@@ -105,7 +112,7 @@ class UsersController {
   };
 
   adminUpdateUser = async (req, res, next) => {
-    const { userId } = req.params
+    const { userId } = req.params;
     const { id, nickname, email, address } = req.body;
     const updateUser = await this.userService.updateUser(
       userId,
