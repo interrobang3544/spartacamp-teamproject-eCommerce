642df0c95d21832e7cefd485f3156496edf52d8c
Jeongjiw00 <wldn0229@naver.com>
2023-02-07 21:00:47 +0900

    fix loginCheck

--- routes/api.routes.js
+++ routes/api.routes.js
@@ -26,23 +26,23 @@ router.get(
   '/kakao/callback',
   //? 그리고 passport 로그인 전략에 의해 kakaoStrategy로 가서 카카오계정 정보와 DB를 비교해서 회원가입시키거나 로그인 처리하게 한다.
   passport.authenticate('kakao', {
-    failureRedirect: '/', // kakaoStrategy에서 실패한다면 실행
+    failureRedirect: '/login', // kakaoStrategy에서 실패한다면 실행
   }),
   // kakaoStrategy에서 성공한다면 콜백 실행
   (req, res) => {
-    console.log(req.user);
     const accessToken = jwt.sign(
       {
+        userId: req.user.userId,
         id: req.user.id,
-        nickname: req.user.nickname,
+        userNickname: req.user.nickname,
       },
-      process.env.KAKAO_SECRET,
+      'my-secrect-key',
       {
         expiresIn: '1d',
       }
     );
     res.cookie('accessToken', accessToken);
-    res.redirect('/');
+    res.redirect('/mypage');
   }
 );
 
