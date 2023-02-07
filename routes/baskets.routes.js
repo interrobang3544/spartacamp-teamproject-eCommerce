e35a3acbe1332dcdc875812ac24a15993b35217c
redfaceman <118166327+redfaceman@users.noreply.github.com>
2023-02-08 01:13:21 +0900

    locals

--- routes/baskets.routes.js
+++ routes/baskets.routes.js
@@ -1,9 +1,11 @@
 const express = require('express');
 const router = express.Router();
 
+const authMiddleware = require('../middlewares/auth');
+
 const BasketsController = require('../controllers/baskets.controller');
 const basketsController = new BasketsController();
 
-router.post('/', basketsController.CreateBasket);
+router.post('/', authMiddleware, basketsController.CreateBasket);
 
 module.exports = router;
